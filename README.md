cdc1bc18816711bda1839170fd0e0065f5e1a8e3
cuppar <cuppar@163.com>
2023-12-14 17:28:39 +0800

    args parse and readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# rdns
+# Rust DNS
+
+A toy DNS by Rust from scratch.
+
+rdns default use port 22222 to open underlying UDP connection.
+
+Usage:
+```
+rdns <domain> <query-type>
+
+    <query-type> is one of:
+        - a
+        - ns
+        - cname
+        - mx
+        - aaaa
+```
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,47 @@
 mod packet;
 
-use std::net::UdpSocket;
+use std::{env, net::UdpSocket, process};
 
 use packet::*;
+use rand::Rng;
 
 fn main() {
-    if let Err(err) = run() {
+    let mut args = env::args();
+    if args.len() < 3 {
+        println!("Usage: rdns <domain> <query-type>");
+        println!();
+        println!("    <query-type> is one of:");
+        println!("        - a");
+        println!("        - ns");
+        println!("        - cname");
+        println!("        - mx");
+        println!("        - aaaa");
+        process::exit(1);
+    }
+
+    let qname = args.nth(1).unwrap();
+    let qtype = match args.next().unwrap().to_lowercase().as_str() {
+        "a" => QueryType::A,
+        "ns" => QueryType::NS,
+        "cname" => QueryType::CNAME,
+        "mx" => QueryType::MX,
+        "aaaa" => QueryType::AAAA,
+        _ => QueryType::UNKNOWN(0),
+    };
+
+    if let Err(err) = run(qname.as_str(), qtype) {
         println!("Error: {}", err);
     }
 }
 
-fn run() -> Result<()> {
-    let qname = "yahoo.com";
-    let qtype = QueryType::MX;
-
+fn run(qname: &str, qtype: QueryType) -> Result<()> {
     let server = ("8.8.8.8", 53);
 
     let socket = UdpSocket::bind(("0.0.0.0", 22222))?;
 
     let mut req_packet = DnsPacket::new();
 
-    req_packet.header.id = 9999;
+    req_packet.header.id = rand::thread_rng().gen();
     req_packet.header.questions = 1;
     req_packet.header.recursion_desired = true;
     req_packet.questions.push(DnsQuestion::new(qname, qtype));
